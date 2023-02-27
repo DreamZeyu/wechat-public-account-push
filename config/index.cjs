32e54f1c593bf2fe53f28909a066c6a59f886641
DreamZeyu <96050173+DreamZeyu@users.noreply.github.com>
2023-02-27 17:30:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o3YX86lCEKW2jlyNEybYrxiILzE0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'AAnJdm9EkceRnoiPWH7kmy1U9yJ-qTlutWR2UZ7oxzU',
+      useTemplateId: 'YKdPy__W3VRKiajniyfJ9yG5YhtsaygsAAXA4_V5vbs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-08',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'AAnJdm9EkceRnoiPWH7kmy1U9yJ-qTlutWR2UZ7oxzU',
+  CALLBACK_TEMPLATE_ID: 'YKdPy__W3VRKiajniyfJ9yG5YhtsaygsAAXA4_V5vbs',
 
   CALLBACK_USERS: [
     {
