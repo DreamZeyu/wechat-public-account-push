f597bb2f903f982c9fe509d38fc9964051a5adc7
DreamZeyu <96050173+DreamZeyu@users.noreply.github.com>
2023-02-28 10:24:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o3YX86lCEKW2jlyNEybYrxiILzE0',
+      id: 'o3YX86otZnxK7_CFAEhws8EWFgio',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'zowkD-vyL3mNFq4fHV6WeWBd6CSi5BN0UrkVjKfApjQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
